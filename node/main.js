3ed51a146ee348d348d06e86cd9609f43d3d195f
uwutut <109206274+uwutut@users.noreply.github.com>
2022-08-17 17:28:51 +0900

    함수 8/17
    asda

--- node/main.js
+++ node/main.js
@@ -1,89 +1,48 @@
-    // 변수 만들기//
-
-    //a = 10
-    //console.log(a)
-
-    //const b = 20 // (정)상수: 한 번 정하면 바뀌지 않음
-    //console.log(b)
-
-    //a = a+40
-    //console.log(a)
-
-   //var num1 = 10
-    //var num2 = 30
-    //var s = `${num1} + ${num2} = ${num1 + num2}` // ` : 포메팅 방식
-    //console.log(s)
-
-    //문자열
-
-    //s = "hello"
-    //console.log(s[0]) // 인덱스
-    //console.log(s.slice(1,3)) // 슬라이싱
-
-    // 리스트
-
-    //a = [1,2,3]
-    //console.log(a)
-
-    //c = Array(10) // 빈 공간에 리스트를 만들 떄 사용한다.
-    //console.log(c)
-
-    // 추가
-    //c.push(5)
-    //console.log(c)
-
-//삭제
- //   a.pop() // 맨 뒤
-  //  console.log(a)
-   // a.shift(5) // 맨 앞
-   // console.log(a)
-
-// 조건
-//a = 10
-//if (a > 10){
-    //console.log("크다")
-//}
-//else if(a < 10){
-    //console.log("작다")
-//}
-//else{
-    //console.log("같다")
-//}
-
-//반복ㄱㄱㄱㄱㄱㄱㄱㄱㄱㄱㄱㄱㄱ
-//for(var i = 0 ; i < 10 ; i++){
-    //console.log(i)
-//}
-//i = 0
-////while(i < 10){
-    //i++ 
-   //console.log(i)
-//}
-
-
-
-//for(var i = 1 ; i < 20; i++) {  
-    //console.log(i + '단')
-    //for(var j = 1; j < 10; j++) {    
-        //console.log(i + ' * ' + j + ' = ' + (i*j));  }
-    //}
-
-
-//sum = 0
-//for(i = 1 ; i <= 3452 ; i++) {
-  //sum += i
-//}
-//console.log(sum)
-
-// 객체
-var 공룡 = {
-    x : 10,
-    y : 20,
-    w : 100,
-    h : 100,
-    draw(){
-        console.log("this.x")
-        console.log("그리기 함수 입니다.")
-    }
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+function templates(title, data1){
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>기본 페이지</title>
+    </head>
+    <body>
+        <h1>${title}</h1>
+        <ul>
+            <li><a href="/?id=html">html</a></li>
+            <li><a href="/?id=css">css</a></li>
+        </ul>
+        <p>${data1}</p>
+    </body>
+    </html>
+    `
 }
-공룡.draw
+
+var app = http.createServer(function(request, response){
+    var path = url.parse(request.url, true).path
+    if(request.url != '/favicon.ico'){
+        var title = url.parse(request.url, true).query
+        var title = title.id
+        if(path === "/"){
+            title = "home"
+            data1 = "welome"
+            console.log(path)
+            response.writeHead(200);
+            response.end(templates(title,data1));
+        }
+
+        else{
+            var data1 = fs.readFileSync(`./data/${title}`,'utf-8');
+            response.writeHead(200);
+            response.end(templates(title,data1))
+        }
+        
+    }
+})
+
+app.listen(80)
